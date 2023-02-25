7615b62b8d3647ba5640ac8dd24e1a8d50ac8615
CrispyCreme10 <46547340+CrispyCreme10@users.noreply.github.com>
2023-02-24 22:21:32 -0600

    filling buckets

--- src/app/components/bucket/bucket.component.ts
+++ src/app/components/bucket/bucket.component.ts
@@ -1,10 +1,32 @@
+import { LineItemService } from './../../services/line-item.service';
+import { LineItem } from './../../types';
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-bucket',
+  selector: '.app-bucket',
   templateUrl: './bucket.component.html',
   styleUrls: ['./bucket.component.css']
 })
 export class BucketComponent {
+  id!: number;
+  total!: number;
+  lineItems: LineItem[] = [];
 
+  constructor(private lineItemService: LineItemService) {}
+
+  ngOnInit() {
+    this.lineItemService.getLineItems().subscribe(items => this.lineItems = items);
+  }
+
+  createLineItem() {
+    this.lineItemService.addLineItem(1).subscribe(item => this.lineItems.push(item));
+  }
+
+  getTotalFormatted(): string {
+    const formatter = new Intl.NumberFormat('en-US', {
+      style: 'currency',
+      currency: 'USD',
+    });
+    return this.total !== undefined ? formatter.format(this.total) : "$0.00";
+  }
 }
--- src/app/services/line-item.service.ts
+++ src/app/services/line-item.service.ts
@@ -1,9 +1,29 @@
+import { Observable } from 'rxjs';
 import { Injectable } from '@angular/core';
+import { LineItem } from '../types';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+const httpOptions = {
+  headers: new HttpHeaders({
+    'Content-Type': 'application/json'
+  })
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class LineItemService {
+  private apiUrl = 'http://localhost:5000/lineitems';
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  getLineItems(): Observable<LineItem[]> {
+    return this.http.get<LineItem[]>(this.apiUrl);
+  }
+
+  addLineItem(bucketId: number): Observable<LineItem> {
+    return this.http.post<LineItem>(this.apiUrl, {
+      bucketId: bucketId
+    }, httpOptions);
+  }
 }
