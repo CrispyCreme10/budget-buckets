acf902002fd51fdc1aa3cc31e85339a8686bf395
CrispyCreme10 <jordanrenner69@gmail.com>
2023-03-03 19:03:06 -0600

    updates to some sidebar stuff

--- src/app/components/simple-view/simple-view.component.ts
+++ src/app/components/simple-view/simple-view.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { faAngleDown, faAngleRight, faEllipsis, faGear, faHeadset, faUser, IconDefinition } from '@fortawesome/free-solid-svg-icons';
+import { Component, Input } from '@angular/core';
+import { faAngleDown, faAngleRight, faCheck, faEllipsis, faGear, faHeadset, faUser, IconDefinition } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-simple-view',
@@ -7,8 +7,18 @@ import { faAngleDown, faAngleRight, faEllipsis, faGear, faHeadset, faUser, IconD
   styleUrls: ['./simple-view.component.css']
 })
 export class SimpleViewComponent {
+  // header icons
   faEllipsis = faEllipsis;
+  faCheck = faCheck;
+
+  // footer icons
   faGear = faGear;
   faHeadset = faHeadset;
   faUser = faUser;
+
+  @Input() showHeaderSettings = false;
+
+  openHeaderSettings() {
+    this.showHeaderSettings = !this.showHeaderSettings;
+  }
 }
