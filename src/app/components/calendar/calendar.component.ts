43c639adc424761aa08fce77437186e7ee76af05
CrispyCreme10 <46547340+CrispyCreme10@users.noreply.github.com>
2023-03-04 23:42:30 -0600

    adding week and day calendar views

--- src/app/components/calendar/calendar.component.ts
+++ src/app/components/calendar/calendar.component.ts
@@ -1,8 +1,6 @@
-import { NgStyle } from '@angular/common';
 import { Component } from '@angular/core';
-import { faAngleLeft, faAngleRight } from '@fortawesome/free-solid-svg-icons';
+import { faAngleLeft, faAngleRight, faCalendarDay, faCalendarDays, faCalendarWeek } from '@fortawesome/free-solid-svg-icons';
 
-const daysInWeek = 7;
 interface CalendarDate {
   date: Date,
   dateStr: string,
@@ -15,14 +13,25 @@ interface CalendarDate {
   styleUrls: ['./calendar.component.css']
 })
 export class CalendarComponent {
+  daysInWeek = 7;
+  MONTH = 'month';
+  WEEK = 'week';
+  DAY = 'day';
+
   currDate = new Date(Date.now());
   weeks: number = 6;
   calendarDates: CalendarDate[] = [];
   dateTextStyles = {};
+  currentView = this.MONTH;
 
   // title icons
   faAngleLeft = faAngleLeft;
   faAngleRight = faAngleRight;
+
+  // calendar view icons
+  faCalendarMonth = faCalendarDays;
+  faCalendarWeek = faCalendarWeek;
+  faCalendarDay = faCalendarDay;
 
   ngOnInit() {
     this.buildCalendarMonth();
@@ -37,7 +46,7 @@ export class CalendarComponent {
     const firstOfMonth = new Date(dateYear, dateMonth, 1)
     const calendarDate = new Date(firstOfMonth);
     calendarDate.setDate(calendarDate.getDate() - firstOfMonth.getDay());
-    for (let count = 0; count < daysInWeek * this.weeks; count++) {
+    for (let count = 0; count < this.daysInWeek * this.weeks; count++) {
       const calendarMonth = calendarDate.getMonth();
       let formattedDate = calendarDate.getDate().toString();
       if (calendarMonth !== dateMonth) {
@@ -56,16 +65,74 @@ export class CalendarComponent {
   }
 
   buildCalendarWeek() {
-
+    this.calendarDates = [];
+    const dateYear = this.currDate.getFullYear();
+    const dateMonth = this.currDate.getMonth();
+    const firstOfMonth = new Date(dateYear, dateMonth, 1)
+    const calendarDate = new Date(firstOfMonth);
+    calendarDate.setDate(calendarDate.getDate() - firstOfMonth.getDay());
+    for (let count = 0; count < this.daysInWeek; count++) {
+      const calendarMonth = calendarDate.getMonth();
+      let formattedDate = calendarDate.getDate().toString();
+      if (calendarMonth !== dateMonth) {
+        const options: Intl.DateTimeFormatOptions = {
+          month: "short"
+        }
+        formattedDate = `${new Intl.DateTimeFormat("en-US", options).format(calendarDate)} ${formattedDate}`;
+      }
+      this.calendarDates.push({
+        date: new Date(calendarDate),
+        dateStr: formattedDate,
+        isInCurrMonth: calendarMonth === dateMonth && calendarDate.getFullYear() === dateYear
+      })
+      calendarDate.setDate(calendarDate.getDate() + 1);
+    }
   }
 
   buildCalendarDay() {
 
   }
 
+  getCalendarViewStyles(view: string): any {
+    return this.currentView === view ? { 'background-color': '#04395e'} : {};
+  }
+
+  setCalendarView(view: string) {
+    if (this.currentView !== view) {
+      console.log(view);
+      switch (view) {
+        case this.MONTH:
+          this.buildCalendarMonth();
+          break;
+        case this.WEEK:
+          this.buildCalendarWeek();
+          break;
+        case this.DAY:
+          this.buildCalendarDay();
+          break;
+        default:
+          throw new Error("Error setting Calendar data");
+      }
+
+      this.currentView = view;
+    }
+  }
+
+  isMonthView(): boolean {
+    return this.currentView === this.MONTH;
+  }
+
+  isWeekView(): boolean {
+    return this.currentView === this.WEEK;
+  }
+
+  isDayView(): boolean {
+    return this.currentView === this.DAY;
+  }
+
+
   isDateToday(dateToCompare: Date): boolean {
     const todaysDate = new Date(Date.now());
-    console.log({dateToCompare, todaysDate});
     return dateToCompare.getFullYear() === todaysDate.getFullYear() &&
           dateToCompare.getMonth() === todaysDate.getMonth() &&
           dateToCompare.getDate() === todaysDate.getDate();
